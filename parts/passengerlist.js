9934b7880b0746088e995dd80e9a62de0ea3a832
John Carlo Villas <johncarlo.villas@evsu.edu.ph>
2021-10-24 14:36:03 +0800

    Removed Line 43-66 btmSheet

--- parts/passengerlist.js
+++ parts/passengerlist.js
@@ -40,30 +40,6 @@ const Passenger = ({item, unload}) => (
   </View>
 );
 
-const [isVisible, setIsVisible] = useState(false);
-const list = [
-  { title: 'List Item 1' },
-  { title: 'List Item 2' },
-  {
-    title: 'Cancel',
-    containerStyle: { backgroundColor: 'red' },
-    titleStyle: { color: 'white' },
-    onPress: () => setIsVisible(false),
-  },
-];
-
-<BottomSheet
-  isVisible={isVisible}
-  containerStyle={{ backgroundColor: 'rgba(0.5, 0.25, 0, 0.2)' }}
->
-  {list.map((l, i) => (
-    <ListItem key={i} containerStyle={l.containerStyle} onPress={l.onPress}>
-      <ListItem.Content>
-        <ListItem.Title style={l.titleStyle}>{l.title}</ListItem.Title>
-      </ListItem.Content>
-    </ListItem>
-  ))}
-</BottomSheet>;
 
 const Passengerlist = ({ws, route, warning}) => {
 
